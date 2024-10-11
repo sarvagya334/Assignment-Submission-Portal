1847a711ca3a8cc86e44c0c6b6834de1b0c50aab
sarvagya334 <49235989+sarvagya334@users.noreply.github.com>
2024-10-11 15:21:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,4 +17,4 @@ app.use('/admins', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.use(express.static(path.join(__dirname, 'frontend')));
+
