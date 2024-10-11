bd9c37dd6f825da89ada1e28d6b3bbbe55e10a2b
sarvagya334 <49235989+sarvagya334@users.noreply.github.com>
2024-10-11 15:21:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,8 +17,4 @@ app.use('/admins', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.get('/', (req, res) => {
-    // Send the HTML file as the response
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
 app.use(express.static(path.join(__dirname, 'frontend')));
